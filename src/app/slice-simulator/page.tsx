96f2ca79b5b8ff062e1b0810c2af523a069647df
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-05-19 11:48:54 -1000

    feat: support Slice Simulator

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -1,14 +1,14 @@
+"use client";
+
 import { SliceSimulator } from "@slicemachine/adapter-next/simulator";
 import { SliceZone } from "@prismicio/react";
 
-import { components } from "../slices";
+import { components } from "@/slices";
 
-const SliceSimulatorPage = () => {
+export default function SliceSimulatorPage() {
   return (
     <SliceSimulator
       sliceZone={(props) => <SliceZone {...props} components={components} />}
     />
   );
-};
-
-export default SliceSimulatorPage;
+}
