f96e5d24430adbdd48a55ecc53624911c2d28777
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-05-19 12:06:57 -1000

    refactor: use a TypeScript Tailwind CSS config file

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/slices/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
