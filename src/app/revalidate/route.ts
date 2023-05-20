8b6ab66f07c04849ede458ae02176ce8524191fe
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-05-19 16:29:29 -1000

    fix: change `/revalidate` to `POST`

--- src/app/revalidate/route.ts
+++ src/app/revalidate/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { revalidateTag } from "next/cache";
 
-export async function GET() {
+export async function POST() {
   revalidateTag("prismic-repository");
 
   return NextResponse.json({ revalidated: true, now: Date.now() });
