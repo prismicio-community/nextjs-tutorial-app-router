c586703d880753f0a52b7e216aff95f3c83f6a35
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-06-09 15:00:04 -1000

    feat: support Draft Mode with `@prismicio/next` alpha

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -34,10 +34,10 @@ const routes: prismic.ClientConfig["routes"] = [
 export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
   const client = prismic.createClient(repositoryName, {
     routes,
-    fetchOptions: {
-      cache: process.env.NODE_ENV === "production" ? "force-cache" : "no-store",
-      next: { tags: ["prismic"] },
-    },
+    fetchOptions:
+      process.env.NODE_ENV === "production"
+        ? { next: { tags: ["prismic"] }, cache: "force-cache" }
+        : { next: { revalidate: 5 } },
     ...config,
   });
 
