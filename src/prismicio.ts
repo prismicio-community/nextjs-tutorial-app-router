849be5299f127aba93141bbc93ddfc88235b3af6
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-06-09 15:45:03 -1000

    chore: re-enable previews

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -41,11 +41,11 @@ export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
     ...config,
   });
 
-  // prismicNext.enableAutoPreviews({
-  //   client,
-  //   previewData: config.previewData,
-  //   req: config.req,
-  // });
+  prismicNext.enableAutoPreviews({
+    client,
+    previewData: config.previewData,
+    req: config.req,
+  });
 
   return client;
 };
